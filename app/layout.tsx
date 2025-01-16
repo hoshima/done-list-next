7ba056692cd0ae19857250209cb3298ca6f5fb62
hoshima <hoshima@users.noreply.github.com>
2025-01-16 21:19:20 +0900

    chore: :heavy_plus_sign: NextUIを追加してみる

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
+import { Providers } from "./providers";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -30,31 +31,33 @@ export default function RootLayout({
   return (
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <main className="flex min-h-screen flex-col items-center">
-            <div className="flex w-full flex-1 flex-col items-center gap-20">
-              <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-                <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
-                  <div className="flex items-center gap-5 font-semibold">
-                    <Link href={"/"}>やったログ</Link>
+        <Providers>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <main className="flex min-h-screen flex-col items-center">
+              <div className="flex w-full flex-1 flex-col items-center gap-20">
+                <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+                  <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
+                    <div className="flex items-center gap-5 font-semibold">
+                      <Link href={"/"}>やったログ</Link>
+                    </div>
+                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
                   </div>
-                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
-                </div>
-              </nav>
+                </nav>
 
-              <div className="container max-w-5xl">{children}</div>
+                <div className="container max-w-5xl">{children}</div>
 
-              <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
-                <ThemeSwitcher />
-              </footer>
-            </div>
-          </main>
-        </ThemeProvider>
+                <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
+                  <ThemeSwitcher />
+                </footer>
+              </div>
+            </main>
+          </ThemeProvider>
+        </Providers>
       </body>
     </html>
   );
