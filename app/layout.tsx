b0eb6a577f69727f99f070e6b08e041b65569fe0
hoshima <hoshima@users.noreply.github.com>
2025-01-16 21:21:25 +0900

    refactor: :recycle: ThemeProviderをproviders.tsxに移動

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 import { Geist } from "next/font/google";
-import { ThemeProvider } from "next-themes";
 import Link from "next/link";
 import "./globals.css";
 import { Providers } from "./providers";
@@ -32,31 +31,24 @@ export default function RootLayout({
     <html lang="en" className={geistSans.className} suppressHydrationWarning>
       <body className="bg-background text-foreground">
         <Providers>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            <main className="flex min-h-screen flex-col items-center">
-              <div className="flex w-full flex-1 flex-col items-center gap-20">
-                <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
-                  <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
-                    <div className="flex items-center gap-5 font-semibold">
-                      <Link href={"/"}>やったログ</Link>
-                    </div>
-                    {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+          <main className="flex min-h-screen flex-col items-center">
+            <div className="flex w-full flex-1 flex-col items-center gap-20">
+              <nav className="flex h-16 w-full justify-center border-b border-b-foreground/10">
+                <div className="flex w-full max-w-5xl items-center justify-between p-3 px-5 text-sm">
+                  <div className="flex items-center gap-5 font-semibold">
+                    <Link href={"/"}>やったログ</Link>
                   </div>
-                </nav>
+                  {!hasEnvVars ? <EnvVarWarning /> : <HeaderAuth />}
+                </div>
+              </nav>
 
-                <div className="container max-w-5xl">{children}</div>
+              <div className="container max-w-5xl">{children}</div>
 
-                <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
-                  <ThemeSwitcher />
-                </footer>
-              </div>
-            </main>
-          </ThemeProvider>
+              <footer className="mx-auto flex w-full items-center justify-center gap-8 border-t py-16 text-center text-xs">
+                <ThemeSwitcher />
+              </footer>
+            </div>
+          </main>
         </Providers>
       </body>
     </html>
