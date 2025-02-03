b4d1bd58303a14bd7d69e904270fb5777a757e10
hoshima <hoshima@users.noreply.github.com>
2025-02-03 01:39:44 +0000

    feat: :sparkles: アカウントページにログアウトボタンを追加

--- app/settings/account/page.tsx
+++ app/settings/account/page.tsx
@@ -1,12 +1,21 @@
 "use client";
 
-import { deleteAccountAction } from "@/app/actions";
+import { signOutAction, deleteAccountAction } from "@/app/actions";
 import { Button } from "@heroui/button";
 
 export default function Account() {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-2xl">アカウント</h1>
+
+      <div className="flex flex-col gap-2">
+        <h2 className="w-full border-b text-xl">ログアウト</h2>
+        <form action={signOutAction}>
+          <Button type="submit" variant="ghost">
+            ログアウト
+          </Button>
+        </form>
+      </div>
 
       <div className="flex flex-col gap-2">
         <h2 className="w-full border-b text-xl">アカウント削除</h2>
