412be333425a5f1980f798ed22788c10b179234b
hoshima <hoshima@users.noreply.github.com>
2025-05-07 14:18:52 +0000

    feat: :lipstick: タスク名のフォントの太さを変更

--- components/task-card.tsx
+++ components/task-card.tsx
@@ -8,7 +8,7 @@ export default function TaskCard({ task }: { task: Task }) {
       key={task.id}
       className="flex flex-col items-start gap-1 rounded-md border border-primary p-4 text-foreground"
     >
-      <h3 className="text-lg font-bold">{task.name}</h3>
+      <h3 className="text-lg font-medium">{task.name}</h3>
       <p className="text-sm">{task.date}</p>
       {task.description ? <p className="text-sm">{task.description}</p> : null}
     </Link>
