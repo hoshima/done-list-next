84f05a79f1b9197f25c71d1ec5449c1b51673099
hoshima <hoshima@users.noreply.github.com>
2025-12-14 16:19:44 +0000

    refactor: :recycle: ProtectedPageのsearchParamsの取得方法を変更し、Searchコンポーネントが早く表示されるように

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,19 +2,19 @@ import FloatingActionButton from "@/components/fab";
 import TaskListSkeleton from "@/components/task-list-skeleton";
 import TaskList from "@/components/task-list";
 import Search from "@/components/ui/search";
-import { Suspense } from "react";
+import { Suspense, use } from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
   title: "ホーム",
 };
 
-export default async function ProtectedPage({
+export default function ProtectedPage({
   searchParams,
 }: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 }) {
-  const params = await searchParams;
+  const params = use(searchParams);
   const query = params["query"]?.toString();
   const page = parseInt(params["page"]?.toString() || "1", 10);
   const suspenseKey = `${query || ""}-${page}`;
@@ -23,7 +23,7 @@ export default async function ProtectedPage({
     <>
       <div className="flex flex-col gap-8 md:gap-20">
         <div className="mx-auto !block w-11/12 md:w-8/12">
-          <Search query={query} />
+          <Search />
         </div>
 
         <Suspense key={suspenseKey} fallback={<TaskListSkeleton />}>
--- components/ui/search.tsx
+++ components/ui/search.tsx
@@ -1,12 +1,19 @@
+"use client";
+
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import { Button } from "@heroui/button";
 import { Input } from "@heroui/input";
 import Form from "next/form";
+import { useSearchParams } from "next/navigation";
 
-export default function Search({ query }: { query?: string }) {
+export default function Search() {
+  const searchParams = useSearchParams();
+  const query = searchParams.get("query") ?? "";
+
   return (
     <Form action="">
       <Input
+        key={query}
         id="query"
         name="query"
         label="検索"
