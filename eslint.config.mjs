d031bdf64895ec3d28f5b6dd817c32dea39684ce
hoshima <hoshima@users.noreply.github.com>
2025-12-29 06:19:38 +0000

    chore: :heavy_minus_sign: eslint/prettierを削除

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,11 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import tailwindcss from "eslint-plugin-tailwindcss";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
-});
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default [
   ...nextCoreWebVitals,
   ...nextTypescript,
-  ...compat.extends("plugin:tailwindcss/recommended"),
   {
-    plugins: {
-      tailwindcss,
-    },
     rules: {
       "@typescript-eslint/no-unused-vars": "warn",
     },
